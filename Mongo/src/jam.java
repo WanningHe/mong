7b61f814b9fc93ba1268115e611f7e3e1150e035
alanh_uxgexei <alanh_uxgexei@DESKTOP-HM6LM7O>
2021-06-12 22:59:02 -0400

    gooey

--- Mongo/src/jam.java
+++ Mongo/src/jam.java
@@ -1,9 +1,105 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+import utils.HSK1Window;
+
+import java.awt.EventQueue;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class jam {
-
+	private static JFrame frame;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("does this work");
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						jam window = new jam();
+						jam.frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+	public jam() {
+		initialize();
 	}
-
-}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(0, 0, 0));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{1112, 0};
+		gridBagLayout.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+	
+		JButton btnNewButton = new JButton("CONFIRM");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			String to = "aminahrizwan00@gmail.com";
+	    String subject = "Hello";
+	    String body = "yam";
+	    String message = "mailto:"+to+"?subject="+subject+"&body="+body+"&attachment=c:/Update8.txt";
+	        URI uri = URI.create(message);
+	        if (Desktop.isDesktopSupported()) {
+	            Desktop desktop = Desktop.getDesktop();
+	            if (desktop.isSupported(Desktop.Action.MAIL)) {
+	                try {
+						desktop.mail(uri);
+					} catch (IOException k) {
+						// TODO Auto-generated catch block
+						k.printStackTrace();
+					} // alternately, pass a mailto: URI in here
+	            }
+			}
+			}
+			});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTH;
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 13;
+		frame.getContentPane().add(btnNewButton, gbc_btnNewButton);
+		frame.setBounds(100, 100, 1126, 843);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+		
+	        
+	
+	     }
+	
+	
+	
